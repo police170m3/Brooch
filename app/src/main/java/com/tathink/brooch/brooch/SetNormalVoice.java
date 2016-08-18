b83431e225f65bd22584e6bbba103556f8ffa562
MSI <170m3@paran.com>
2016-08-18 23:41:57 +0900

    2016.08.18_03 - modify SetNormalVoice Classes

--- app/src/main/java/com/tathink/brooch/brooch/SetNormalVoice.java
+++ app/src/main/java/com/tathink/brooch/brooch/SetNormalVoice.java
@@ -1,7 +1,13 @@
 package com.tathink.brooch.brooch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import org.florescu.android.rangeseekbar.RangeSeekBar;
 
 /**
  * Created by MSI on 2016-08-18.
@@ -11,5 +17,35 @@ public class SetNormalVoice extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_normalvoice);
+
+        // Setup the new range seek bar
+        RangeSeekBar rangeSeekBar = new RangeSeekBar(this);
+        // Set the range
+        rangeSeekBar.setTextAboveThumbsColor(R.color.common_signin_btn_dark_text_default);
+        rangeSeekBar.setRangeValues(0, 300);
+        rangeSeekBar.setSelectedMinValue(20);
+        rangeSeekBar.setSelectedMaxValue(88);
+
+        // Add to layout
+        LinearLayout layout = (LinearLayout) findViewById(R.id.seekbar_placeholder);
+        layout.addView(rangeSeekBar);
+
+        ((Button)findViewById(R.id.setnormalvoice_btn_set)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //측정된 값으로 설정하기
+            }
+        });
+
+        ((Button)findViewById(R.id.setnormalvoice_btn_remeasure)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //다시 측정하기 - 평소 목소리
+                Intent i = new Intent(SetNormalVoice.this, SetCall.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
+
     }
 }

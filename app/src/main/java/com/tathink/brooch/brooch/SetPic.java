ee6b6c88390a386b6ce3f5324144b9a9f09b7306
MSI <170m3@paran.com>
2016-08-23 15:39:05 +0900

    2016.08.23 - modify SetPic Class for Gallery - add ImageUtil Class for Gallery

--- app/src/main/java/com/tathink/brooch/brooch/SetPic.java
+++ app/src/main/java/com/tathink/brooch/brooch/SetPic.java
@@ -1,7 +1,9 @@
 package com.tathink.brooch.brooch;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,12 +15,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -28,23 +33,29 @@ import java.io.IOException;
  */
 public class SetPic extends Activity {
 
-    private static final int    SELECT_PICTURE = 1;
+    private static final int SELECT_PICTURE = 1;
     String selectedImagePath;
     private ImageView imageView1, imageView2, imageView3, imageView4, imageView5;
     private int FLAG = 0;
-    public static final int REQUEST_CAMERA = 9000;
-    public static final int REQUEST_GALLERY = 9001;
+    public static final int REQUEST_CODE_PICKALBUM = 101;
+    private String mImgPath;
+    private int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    String TAG = "HELLO SEJIN~~";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_pic);
 
-        imageView1 = (ImageView)findViewById(R.id.setPicimageView1);
-        imageView2 = (ImageView)findViewById(R.id.setPicimageView2);
-        imageView3 = (ImageView)findViewById(R.id.setPicimageView3);
-        imageView4 = (ImageView)findViewById(R.id.setPicimageView4);
-        imageView5 = (ImageView)findViewById(R.id.setPicimageView5);
+        imageView1 = (ImageView) findViewById(R.id.setPicimageView1);
+        imageView2 = (ImageView) findViewById(R.id.setPicimageView2);
+        imageView3 = (ImageView) findViewById(R.id.setPicimageView3);
+        imageView4 = (ImageView) findViewById(R.id.setPicimageView4);
+        imageView5 = (ImageView) findViewById(R.id.setPicimageView5);
+
+        //mashowmallow permission////////////////////////////////////////////////////////////////////
+        checkPermission();
+        //mashowmallow permission////////////////////////////////////////////////////////////////////
 
         //ImageView 버튼 처리
         View mViewPic1 = findViewById(R.id.setPicimageView1);
@@ -53,10 +64,9 @@ public class SetPic extends Activity {
             public void onClick(View view) {
                 //ImageView 클릭시 이벤트 처리........
                 FLAG = 1;
-                Intent intent1 = new Intent();
-                intent1.setType("image/*");
-                intent1.setAction(Intent.ACTION_GET_CONTENT);    //only 1 get image
-                startActivityForResult(intent1, SELECT_PICTURE);
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(Intent.createChooser(intent, "앨범에서 불러오기"), REQUEST_CODE_PICKALBUM);
             }
         });
 
@@ -66,10 +76,9 @@ public class SetPic extends Activity {
             public void onClick(View view) {
                 //ImageView 클릭시 이벤트 처리........
                 FLAG = 2;
-                Intent intent2 = new Intent();
-                intent2.setType("image/*");
-                intent2.setAction(Intent.ACTION_GET_CONTENT);    //only 1 get image
-                startActivityForResult(intent2, SELECT_PICTURE);
+                Intent intent2 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent2.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(Intent.createChooser(intent2, "앨범에서 불러오기"), REQUEST_CODE_PICKALBUM);
             }
         });
 
@@ -79,10 +88,9 @@ public class SetPic extends Activity {
             public void onClick(View view) {
                 //ImageView 클릭시 이벤트 처리........
                 FLAG = 3;
-                Intent intent3 = new Intent();
-                intent3.setType("image/*");
-                intent3.setAction(Intent.ACTION_GET_CONTENT);    //only 1 get image
-                startActivityForResult(intent3, SELECT_PICTURE);
+                Intent intent3 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent3.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(Intent.createChooser(intent3, "앨범에서 불러오기"), REQUEST_CODE_PICKALBUM);
             }
         });
 
@@ -92,10 +100,9 @@ public class SetPic extends Activity {
             public void onClick(View view) {
                 //ImageView 클릭시 이벤트 처리........
                 FLAG = 4;
-                Intent intent4 = new Intent();
-                intent4.setType("image/*");
-                intent4.setAction(Intent.ACTION_GET_CONTENT);    //only 1 get image
-                startActivityForResult(intent4, SELECT_PICTURE);
+                Intent intent4 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent4.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(Intent.createChooser(intent4, "앨범에서 불러오기"), REQUEST_CODE_PICKALBUM);
             }
         });
 
@@ -105,141 +112,88 @@ public class SetPic extends Activity {
             public void onClick(View view) {
                 //ImageView 클릭시 이벤트 처리........
                 FLAG = 5;
-                Intent intent5 = new Intent();
-                intent5.setType("image/*");
-                intent5.setAction(Intent.ACTION_GET_CONTENT);    //only 1 get image
-                startActivityForResult(intent5, SELECT_PICTURE);
+                Intent intent5 = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent5.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(Intent.createChooser(intent5, "앨범에서 불러오기"), REQUEST_CODE_PICKALBUM);
             }
         });
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                if (Build.VERSION.SDK_INT < 19) {
-                    selectedImagePath = getPath(selectedImageUri);
-                    Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICKALBUM) {
+            if (resultCode == RESULT_OK) {
+                // 앨범인 경우
+                Uri mImageUri = data.getData();
 
-                    if(FLAG == 1) {
-                        imageView1.setImageBitmap(bitmap);
-                    } else if(FLAG == 2){
-                        imageView2.setImageBitmap(bitmap);
-                    } else if(FLAG == 3){
-                        imageView3.setImageBitmap(bitmap);
-                    } else if(FLAG == 4){
-                        imageView4.setImageBitmap(bitmap);
-                    } else if(FLAG == 5){
-                        imageView5.setImageBitmap(bitmap);
-                    }
-
-                    String sjTag = "xxxxxxxxxxxxxxxxxxxxxx";
-                    Log.i(sjTag, selectedImagePath.toString());
-                }
-                else {
-                    ParcelFileDescriptor parcelFileDescriptor;
-                    try {
-                        /*
-                        parcelFileDescriptor = getContentResolver().openFileDescriptor(selectedImageUri, "r");
-                        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-                        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-                        parcelFileDescriptor.close();
-                        */
-
-                        AssetFileDescriptor afd = getContentResolver().openAssetFileDescriptor(selectedImageUri, "r");
-                        BitmapFactory.Options opt = new BitmapFactory.Options();
-                        opt.inSampleSize = 4;
-                        Bitmap image = BitmapFactory.decodeFileDescriptor(afd.getFileDescriptor(), null, opt);
-
-                        //rotate image////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        selectedImagePath = selectedImageUri.getPath();
-                        ExifInterface exif = new ExifInterface(selectedImagePath);
-                        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-                        int exifDegree = exifOrientationToDegree(exifOrientation);
-                        image = rotate(image, exifDegree);
-                        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-                        if(FLAG == 1) {
-                            imageView1.setImageBitmap(image);
-                        } else if(FLAG == 2){
-                            imageView2.setImageBitmap(image);
-                        } else if(FLAG == 3){
-                            imageView3.setImageBitmap(image);
-                        } else if(FLAG == 4){
-                            imageView4.setImageBitmap(image);
-                        } else if(FLAG == 5){
-                            imageView5.setImageBitmap(image);
-                        }
-
-                        String sjTag = "yyyyyyyyyyyyyyyyyyyyyy";
-                        Log.i(sjTag, selectedImagePath.toString());
-
-
-
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (Exception e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-        }
-    }
-
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-        if( uri == null ) {
-            return null;
-        }
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        return uri.getPath();
-    }
-
-    public static int exifOrientationToDegree(int exifOrientation){
-        int value = 0;
-        String TAG = "HELLO WORLD";
-        Log.d(TAG, Integer.toString(exifOrientation));
-        if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_90){
-            Log.i(TAG, "ORIENTATION_ROTATE_90");
-            value = 90;
-        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_180){
-            Log.i(TAG, "ORIENTATION_ROTATE_180");
-            value = 180;
-        } else if(exifOrientation == ExifInterface.ORIENTATION_ROTATE_270){
-            Log.i(TAG, "ORIENTATION_ROTATE_270");
-            value = 270;
-        }
-        return value;
-    }
-
-    public static Bitmap rotate(Bitmap bitmap, int degree){
-        if(degree != 0 && bitmap != null){
-            Matrix m = new Matrix();
-            m.setRotate(degree, (float)bitmap.getWidth() / 2, (float)bitmap.getHeight() / 2);
-
-            try{
-                Bitmap converted = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
-                if(bitmap != converted){
-                    bitmap.recycle();;
-                    bitmap = converted;
-                }
-            } catch (OutOfMemoryError ex){
-                ex.printStackTrace();
+                // 이미지 Path 취득
+                mImgPath = getPath(mImageUri);
+                updateImageView();
             }
         }
-        return bitmap;
     }
+
+    private String getPath(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
+    }
+
+    private void updateImageView() {
+        int degree = ImageUtil.GetExifOrientation(mImgPath);
+        Bitmap resizeBitmap = ImageUtil.loadBackgroundBitmap(SetPic.this, mImgPath);
+        Bitmap rotateBitmap = ImageUtil.GetRotatedBitmap(resizeBitmap, degree);
+        Bitmap roundBitmap = ImageUtil.getRoundedCornerBitmap(rotateBitmap);
+
+        if(FLAG == 1) {
+            imageView1.setImageBitmap(roundBitmap);
+        } else if(FLAG == 2) {
+            imageView2.setImageBitmap(roundBitmap);
+        } else if(FLAG == 3) {
+            imageView3.setImageBitmap(roundBitmap);
+        } else if(FLAG == 4) {
+            imageView4.setImageBitmap(roundBitmap);
+        } else if(FLAG == 5) {
+            imageView5.setImageBitmap(roundBitmap);
+        }
+
+        resizeBitmap.recycle();
+    }
+
+    private void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없을 경우
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // 사용자가 임의로 권한을 취소시킨 경우
+                // 권한 재요청
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                // 권한 요청 (최초 요청)
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의 및 로직 처리
+                    Log.e(TAG, ">>> 동의함.");
+                } else {
+                    // 동의 안함
+                    Log.e(TAG, ">>> 동의를 해주셔야 합니다.");
+                }
+                return;
+            }
+            // 예외 케이스
+        }
+    }
+
 }
